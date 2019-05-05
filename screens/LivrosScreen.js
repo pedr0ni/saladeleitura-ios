ddc2d831e8220d9df3ac1450ef36ce3c17f87078
Matheus Pedroni <pedroni.dev@gmail.com>
2019-05-05 18:52:57 -0300

    API :)

--- screens/LivrosScreen.js
+++ screens/LivrosScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ScrollView, StyleSheet, View, Text } from 'react-native';
+import { ScrollView, StyleSheet, View, Text, ActivityIndicator } from 'react-native';
 import { Card, CardItem, Form, Item, Input, Button, List, ListItem, Left, Thumbnail, } from 'native-base';
 import Title from '../components/Title';
 
@@ -7,18 +7,74 @@ export default class LivrosScreen extends React.Component {
 
   static navigationOptions = {
     header: null
-}
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      buscaText: '',
+      isLoading: true
+    };
+  }
+ 
+  buscarClick() {
+    this.setState({buscaText: this.state.buscaText, isLoading: true});
+  }
+
+  loadLivros(query) {
+    try {
+      fetch(
+        'https://saladeleitura.com.br/app/api.php?action=searchLivro&titulo='+query,
+      ).then(response => response.json())
+      .then(responseJson => {
+        this.livrosArray = responseJson.livros;
+        this.setState({
+          isLoading: false
+        });
+      });
+      
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   render() {
+
+    let Lista;
+
+    if (this.state.buscaText != '') {
+
+      this.loadLivros(this.state.buscaText);
+
+      if (this.state.isLoading) {
+        Lista = (
+          <ActivityIndicator size="large" style={{marginTop: '50%'}} />
+        );
+      } else {
+        Lista = (
+          <List button={true}>
+            {this.livrosArray.map((entry) => {
+              return (
+                <ListItem>
+                  <Text>{entry}</Text>
+                </ListItem>
+              );
+            })}  
+          </List>
+        );
+      }
+      
+    }
+
     return (
       <View style={styles.container}>
 			  <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
           <Title>Livros</Title>
           <Form>
             <Item style={styles.input}>
-              <Input placeholder="Buscar livro..."/>
+              <Input onChangeText={(text) => {this.state.buscaText = text}} ref="busca" placeholder="Buscar livro..."/>
             </Item>
-            <Button style={styles.button} primary>
+            <Button onPress={this.buscarClick.bind(this)} style={styles.button} primary>
               <Text style={{
                 color: 'white',
                 width: '100%',
@@ -27,16 +83,7 @@ export default class LivrosScreen extends React.Component {
               }}>Buscar</Text>
             </Button>
           </Form>
-          <List button={true}>
-            <ListItem thumbnail>
-              <Left>
-                <Thumbnail circular  />
-              </Left>
-            </ListItem>
-            <ListItem button={true}>
-              <Text>Livro Teste</Text>
-            </ListItem>
-          </List>
+          {Lista}
 			  </ScrollView>
 	    </View>
     );
